d61eb1d49a3545051b0240353b014abf5eeca18c
JR <rvictori@emich.edu>
2019-04-03 03:11:58 -0400

    Update section-a.js

--- frontend/_resources/js/section-a.js
+++ frontend/_resources/js/section-a.js
@@ -94,7 +94,7 @@ function getOutput() {
 
     output += '<div class="collection">';
     for (var j = 0; j < answers.length; j++) {
-      output += '<input type="radio" class="collection-item" /><strong>' + String.fromCharCode(65 + j) + '</strong>: ' + answers[j] + '<br />';
+      output += '<input type="radio" name="question-' + i + 'class="collection-item" /><strong>' + String.fromCharCode(65 + j) + '</strong>: ' + answers[j] + '<br />';
     }
     output += '</div>';
 
