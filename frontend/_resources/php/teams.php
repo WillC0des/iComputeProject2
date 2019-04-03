5c1324611a4810f01257cece13208fe0e284b85c
jeff <jgolonka@emich.edu>
2019-04-03 01:57:34 -0400

    teams.csv reading

--- frontend/_resources/php/teams.php
+++ frontend/_resources/php/teams.php
@@ -1,1 +1,24 @@
-<
+<?php
+$csvFile = "../csv/Teams.csv";
+
+$fh = fopen($csvFile, 'r')
+or die ('Error occurred when opening ' . $csvFile);
+
+$data = array();
+while($rec = fgetcsv($fh)){
+    $data[] = $rec;
+}
+
+fclose($fh);
+
+var_dump
+
+
+// open csv file for write
+$fh = fopen('Teams.csv','w');
+ 
+foreach ($data as $rec) {
+ fputcsv($fh, $rec);
+}
+ 
+fclose($fh);
